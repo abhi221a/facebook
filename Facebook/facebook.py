d28692b3c3ea3d9e1a21b9015945dab14530ac48
abhi221a <goswamiabhinava@gmail.com>
2021-06-11 21:09:30 +0530

    scrapping

--- Facebook/facebook.py
+++ Facebook/facebook.py
@@ -7,9 +7,7 @@ Created on Sat Jun  5 15:11:32 2021
 """
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 from bs4 import BeautifulSoup
 import  datetime
 from datetime import timedelta
@@ -21,11 +19,12 @@ import re
 
 searchUser=['Himadri sarmah']
 users=[     'abhinava.goswami.35']
-Output_MM=[]
+Output=[]
 path1=['https://m.facebook.com/iamSagardeepTalukdar/posts/3320023114720980','https://m.facebook.com/himantabiswasarma/posts/10158560131803983']
 i=0
 t=1
 A=1
+
 for path in path1:
     postText =[]
     comments=[]
@@ -34,6 +33,8 @@ for path in path1:
     soup1=BeautifulSoup(browser.page_source,"html.parser")
     pt=soup1.find("div",{"class":"_5rgt _5nk5"}).text
     postText.append(pt)
+    Output.append("Post Text")
+    Output.appent(postText)
     try:
         loadMore=browser.find_element_by_class_name("_108_")
         
@@ -45,3 +46,12 @@ for path in path1:
     pt1=soup1.findAll("div",{"class":"_2b06"})
     for i in pt1:
         comments.append(i.text)
+    
+    Output.append("Comments")
+    Output.appent(comments)
+    A+=1
+    
+df=pd.DataFrame(Output)
+with open("/home/abhi/Desktop/"+A, "w") as json_file:
+        json.dump(Output, json_file)
+browser.quit()
