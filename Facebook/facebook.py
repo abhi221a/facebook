37a05ea83c79733f26b6ab73d3c31d69960d1545
abhi221a <goswamiabhinava@gmail.com>
2021-06-11 21:52:06 +0530

    Scrapping

--- Facebook/facebook.py
+++ Facebook/facebook.py
@@ -21,6 +21,7 @@ users=[     'abhinava.goswami.35']
 path1=['https://m.facebook.com/iamSagardeepTalukdar/posts/3320023114720980','https://m.facebook.com/himantabiswasarma/posts/10158560131803983']
 i=0
 t=1
+t1=1
 A=1
 
 for path in path1:
@@ -38,13 +39,44 @@ for path in path1:
         loadMore=browser.find_element_by_class_name("_108_")
         
         while loadMore == True:
-            
+            t+=1
             loadMore.click()
     except:
         print("no more to load")
+    soup1=BeautifulSoup(browser.page_source,"html.parser")
     pt1=soup1.findAll("div",{"class":"_2b06"})
+    try:
+        morereply=browser.find_element_by_class_name("_4ayk")
+        
+        while morereply == True:
+            t1+=1
+            morereply.click()
+        soup1=BeautifulSoup(browser.page_source,"html.parser")
+        try:
+            reply=soup1.findAll("div",{"class":"_2b1k"}) 
+        except:
+            print("")
+    except:
+        print("no replies")
+    
+    
+    z1=0 
     for i in pt1:
         comments.append(i.text)
+        try:
+             z=reply[z1]
+        except:
+             print('')
+             if z1>=(len(reply)-1):
+                 break
+        reply1=z.findAll("div",{"class":"_2b04"})
+        for j in reply1:
+            nested=[]
+            Output.append(j.text)
+            
+            
+            if z1>=(len(reply)-1):
+                break
     
     Output.append("Comments")
     Output.append(comments)
